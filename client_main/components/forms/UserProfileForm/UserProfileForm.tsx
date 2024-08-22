41976bc2e06e6b0e28b617cbf835d66eb94c6923
lakshya-26 <lakshyakumarsingh.2003.va@gmail.com>
2024-08-22 12:51:09 +0530

    "Updated UserProfileForm component with new imports, added user profile picture display, and refactored form layout and fields."

--- client_main/components/forms/UserProfileForm/UserProfileForm.tsx
+++ client_main/components/forms/UserProfileForm/UserProfileForm.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import LoadingButton from "@/components/LoadingButton";
 import { Button } from "@/components/ui/button";
 import {
@@ -12,7 +12,10 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { User } from "@/lib/types";
+import { useUser } from "@auth0/nextjs-auth0/client";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { CircleUserRound } from "lucide-react";
+import Image from "next/image";
 import { useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
@@ -35,7 +38,15 @@ type Props = {
   buttonText?: string;
 };
 
-const UserProfileForm = ({ onSave, isLoading, currentUser, title="User Profile Form", buttonText="Submit" }: Props) => {
+const UserProfileForm = ({
+  onSave,
+  isLoading,
+  currentUser,
+  title = "User Profile Form",
+  buttonText = "Submit",
+}: Props) => {
+  const { user } = useUser();
+
   const form = useForm<UserformData>({
     resolver: zodResolver(formSchema),
     defaultValues: currentUser,
@@ -43,102 +54,120 @@ const UserProfileForm = ({ onSave, isLoading, currentUser, title="User Profile F
 
   useEffect(() => {
     form.reset(currentUser);
-  }, [currentUser, form])
+  }, [currentUser, form]);
 
   return (
-    <div>
+    <div className="">
       <Form {...form}>
-      <form
-        onSubmit={form.handleSubmit(onSave)}
-        className="space-y-4 bg-gray-50 rounded-lg md:p-10 px-6"
-      >
-        <div>
-          <h2 className="text-4xl font-bold text-center text-[#1e733d] p-4 heading">{title}</h2>
-          <FormDescription>
-            View and change your profile information here
-          </FormDescription>
-        </div>
+        <form
+          onSubmit={form.handleSubmit(onSave)}
+          className="space-y-4 bg-gray-50 rounded-lg md:p-10 px-6"
+        >
+          <div>
+            <h2 className="text-4xl font-bold text-center text-[#1e733d] p-4 heading">
+              {title}
+            </h2>
+            <FormDescription>
+              View and change your profile information here
+            </FormDescription>
+          </div>
 
-        <FormField
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Email</FormLabel>
-              <FormControl>
-                <Input {...field} disabled className="bg-white" />
-              </FormControl>
-            </FormItem>
+          <div>
+            {user?.picture ? (
+              <Image
+                src={user.picture}
+                alt={user.name || "User"}
+                width={100}
+                height={100}
+                className="rounded-full"
+              />
+            ) : (
+              <CircleUserRound className="text-[#46484a]" />
+            )}
+          </div>
+
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input {...field} disabled className="bg-white" />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <Input {...field} disabled className="bg-white" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <div className="flex flex-col md:flex-row gap-4">
+            <FormField
+              control={form.control}
+              name="addressLine1"
+              render={({ field }) => (
+                <FormItem className="flex-1">
+                  <FormLabel>Address</FormLabel>
+                  <FormControl>
+                    <Input {...field} className="bg-white" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="city"
+              render={({ field }) => (
+                <FormItem className="flex-1">
+                  <FormLabel>City</FormLabel>
+                  <FormControl>
+                    <Input {...field} className="bg-white" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="country"
+              render={({ field }) => (
+                <FormItem className="flex-1">
+                  <FormLabel>Country</FormLabel>
+                  <FormControl>
+                    <Input {...field} className="bg-white" />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+
+          <div className="md:pb-0 pb-4">
+          {isLoading ? (
+            <LoadingButton />
+          ) : (
+            <Button type="submit" className="bg-[#1e733d]">
+              {buttonText}
+            </Button>
           )}
-        />
-
-        <FormField
-          control={form.control}
-          name="name"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Name</FormLabel>
-              <FormControl>
-                <Input {...field} disabled className="bg-white" />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-
-        <div className="flex flex-col md:flex-row gap-4">
-          <FormField
-            control={form.control}
-            name="addressLine1"
-            render={({ field }) => (
-              <FormItem className="flex-1">
-                <FormLabel>Address</FormLabel>
-                <FormControl>
-                  <Input {...field} className="bg-white" />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-
-          <FormField
-            control={form.control}
-            name="city"
-            render={({ field }) => (
-              <FormItem className="flex-1">
-                <FormLabel>City</FormLabel>
-                <FormControl>
-                  <Input {...field} className="bg-white" />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-
-          <FormField
-            control={form.control}
-            name="country"
-            render={({ field }) => (
-              <FormItem className="flex-1">
-                <FormLabel>Country</FormLabel>
-                <FormControl>
-                  <Input {...field} className="bg-white" />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-        </div>
-
-        {isLoading ? (
-          <LoadingButton />
-        ) : (
-          <Button type="submit" className="bg-[#1e733d]">
-            {buttonText}
-          </Button>
-        )}
-      </form>
-    </Form>
+          </div>
+        </form>
+      </Form>
     </div>
   );
 };
